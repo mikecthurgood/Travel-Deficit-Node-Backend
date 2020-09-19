604e48d1f8db46b33125a3f268795152bc15689b
Mike Thurgood <mike.thurgood@wintechnologies.net>
2020-09-19 14:16:44 +0100

    update db url

--- db/tdDb.js
+++ db/tdDb.js
@@ -1,22 +1,22 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-module.exports = new Sequelize(process.env.DBNAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
-    dialect: "postgres",
-    host: "localhost",
-    port: 5432,
-    storage: "./session.sqlite"
-})
-
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     protocol: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false
-//         }
-//       },
+// module.exports = new Sequelize(process.env.DBNAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
+//     dialect: "postgres",
+//     host: "localhost",
+//     port: 5432,
 //     storage: "./session.sqlite"
 // })
+
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+    storage: "./session.sqlite"
+})
 
