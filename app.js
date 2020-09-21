0219d0f41f1a020bbbcf2358bfdd94396185028d
Mike Thurgood <mike.thurgood@wintechnologies.net>
2020-09-22 00:25:37 +0100

    add cors

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ const port = process.env.PORT;
 
 const app = express()
 const cors = require('cors');
+
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain)
 app.use(cors())
 app.use(bodyParser())
 app.use(auth)
